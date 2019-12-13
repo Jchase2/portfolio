0f3667be5777fbb743ee5f3042635e848e2fba4f
JChase2 <mr.jdchase@gmail.com>
2019-12-13 16:07:55 -0500

    Added new cards and images for additional certificates, fixed some minor stuff wrt spacing.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+//import '@fortawesome/fontawesome-free/js/brands';
 import './style.css';
 console.log("Hello!")
