795f7cdb30b1b423ac1abd3c37160deca896e5ac
JChase2 <mr.jdchase@gmail.com>
2020-01-10 01:26:21 -0500

    Added new CSS with media class to use a bigger modal, also fixed some margins for buttons.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/dropdown';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
